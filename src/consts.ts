304bfc4552208020aa515c82a2e50ea16cc27659
Tyler Jackson <61607534+TylerJacksonGit@users.noreply.github.com>
2026-03-17 22:44:12 -0600

    Delete V1

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Home Base Links";
-export const SITE_DESCRIPTION = "Tyler's homebase linker page";
